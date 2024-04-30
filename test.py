e3484bd77df500996d1d2b4bab046296627b9a75
Rahim Hossain <rhossain@binghamton.edu>
2024-04-30 13:10:46 -0400

    Data Parallelism added

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-import torchaudio
-
-torchaudio.set_audio_backend('soundfile')
-try:
-    waveform, sample_rate = torchaudio.load('../Data/LibriSpeech/train-clean-100/103/1240/103-1240-0000.flac')
-    print("Audio loaded successfully:", waveform.shape, sample_rate)
-except Exception as e:
-    print("Failed to load audio:", e)
+import torch
+print(torch.cuda.is_available())
